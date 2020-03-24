3526280e60febf9715bc1b9f7e555b521292abbf
Adebisi Ayomide <usiwoma.oghenovo@rosabon-finance.net>
2020-03-25 00:42:32 +0100

    added auth for verifwig jwt token

--- backend/hashV/schema.py
+++ backend/hashV/schema.py
@@ -9,7 +9,9 @@ class Query(blog.queries.Query,users.queries.Query, graphene.ObjectType):
   pass
 
 class Mutation(blog.mutations.Mutation, users.mutations.Mutation, graphene.ObjectType):
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 
 schema = graphene.Schema(query=Query,mutation=Mutation)
